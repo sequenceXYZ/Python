5e545ac951c67dfbf94fca5b38b3241913984346
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2023-06-18 14:04:23 +0300

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+"""
+Write a program that prints "Hello, World!" to the console.
+"""
+"""
+Create a new file called hello_world.py.
+In the file, write a single line of code that prints the string "Hello, World!" to the console.
+Run the file to verify that it prints the expected output.
+"""
+
+print("Hello, World!")
